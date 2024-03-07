74554ccd2e19b172e9d1e915b6dbe4fff1a914c5
alireza375 <126044981+alireza375@users.noreply.github.com>
2024-03-07 12:27:13 +0600

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Http\Controllers\Api\BannerController;
+
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,4 +14,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::post('/banner-page', [BannerController::class, 'BannerPage']);
+
